5f9c06fbec03f21b2b318c139c9723fc93e8f598
Santiago Vanegas <sntg.vanegas@gmail.com>
2017-04-09 07:56:02 -0400

    Started going through tutorials
    Started going through
    https://forum.unity3d.com/threads/unity-5-fps-tutorials-gtgd-s3-advanced-first-person-shooter.360105/
    Got up to raycasting, if I"m not mistaken.

---
+++ Beef Jerky II/Assets/Scripts/Shoot.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+    private float nextFire = 0f;
+    private float rate = 0.25f;
+    private RaycastHit hit;
+    private float range = 300;
+    private Transform myTransform;
+
+	// Use this for initialization
+	void Start () {
+        myTransform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        checkForInput();
+	}
+
+    void checkForInput()
+    {
+        if(Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + rate;
+
+            if(Physics.Raycast(myTransform.position, myTransform.forward, out hit, range))
+            {
+                if(hit.transform.CompareTag("Enemy"))
+                    Debug.Log("Enemy " + hit.transform.name);
+            }
+        }
+    }
+}
---
+++ Beef Jerky II/Assets/Scripts/Welcome.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Welcome : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(WelcomeMessage());
+	}
+
+    ////Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+
+    IEnumerator WelcomeMessage ()
+    {
+        yield return new WaitForSeconds(4.5f);
+        Debug.Log("Welcome!");
+    }
+
+     
+}
